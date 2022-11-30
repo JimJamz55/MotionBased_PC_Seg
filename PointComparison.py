4b3b8bbef08b0973e03cd5c98adaf774b7146eac
clee2023 <clee2@wpi.edu>
2022-11-29 23:37:44 -0500

    segment big boy

--- PointComparison.py
+++ PointComparison.py
@@ -120,22 +120,66 @@ def sparse_subset3(points,colors,r):
 
     return result, resultC
 
-def differencePoints(source, target,r):
+def differencePoints(source, target, padding):
     source_temp = copy.deepcopy(source)
-    target_temp = copy.deepcopy(target)
-    points1 = np.asarray(source_temp.points)
-    points2 = np.asarray(target_temp.points)
-    colors1 = np.asarray(source_temp.colors)
-    colors2 = np.asarray(target_temp.colors)
-    test = source.compute_point_cloud_distance(target)
+    points = np.asarray(source_temp.points)
+    colors = np.asarray(source_temp.colors)
+    distances = source.compute_point_cloud_distance(target)
     listOfRedPoints = []
     listOfRedColors = []
-    for i, val in enumerate(test):
-        if val > r:
-            listOfRedPoints.append(points1[i])
-            listOfRedColors.append(colors1[i])
+    twentyfifth, seventyfifth = np.quantile(distances, [0.25,0.75])
+    iqr = seventyfifth-twentyfifth
+    upperFence = seventyfifth+3*iqr
+    for i, val in enumerate(distances):
+        if val > upperFence + padding:
+            listOfRedPoints.append(points[i])
+            listOfRedColors.append(colors[i])
 
     return listOfRedPoints, listOfRedColors
+
+def ransacDB(pcd):
+    plane_model, inliers = pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=1000)
+    # inlier_cloud = pcd.select_by_index(inliers)
+    # outlier_cloud = pcd.select_by_index(inliers, invert=True)
+    # inlier_cloud.paint_uniform_color([1, 0, 0])
+    # outlier_cloud.paint_uniform_color([0.6, 0.6, 0.6])
+    # o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+
+    labels = np.array(pcd.cluster_dbscan(eps=0.05, min_points=0))
+    max_label = labels.max()
+    colors = plt.get_cmap("tab20")((labels / (max_label if max_label > 0 else 1)))
+
+    unique, counts = np.unique(labels, return_counts=True)
+    order_labels = np.argsort(counts)
+    # colors[labels < 0] = 0
+    # colors[labels != 909090909090909090] = 0
+    # colors[labels == order_labels[-1]] = [0.0356, 0.096, 0.043, 0]
+    # colors[labels == order_labels[-2]] = [0.01234, 0.1234, 0.09382, 0]
+    colors[labels != order_labels[-1]] = 0
+
+    # colors1 = colors[labels != order_labels[-1]] = 0
+    # colors2 = colors[labels != order_labels[-2]] = 0
+    # colors = colors1+colors2
+    # for num, i in enumerate(labels):
+    #     if i != order_labels[-1] or i != order_labels[-2]:
+    #         colors[num] = 0
+    pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
+
+    # segment_models = {}
+    # segments = {}
+    # max_plane_idx = 20
+    # rest = pcd
+    # for i in range(max_plane_idx):
+    #     colors = plt.get_cmap("tab20")(i)
+    #     segment_models[i], inliers = rest.segment_plane(
+    #         distance_threshold=0.01, ransac_n=3, num_iterations=1000)
+    #     segments[i] = rest.select_by_index(inliers)
+    #     labels = np.array(segments[i].cluster_dbscan(eps=d_threshold * 10, min_points=10))
+    #     segments[i].paint_uniform_color(list(colors[:3]))
+    #     rest = rest.select_by_index(inliers, invert=True)
+    #     print("pass", i, "/", max_plane_idx, "done.")
+
+    return pcd
 
 if __name__ == "__main__":
     source = o3d.io.read_point_cloud("ObjMoveEx/bottle1.ply")
@@ -143,8 +187,9 @@ if __name__ == "__main__":
     # p3_load, p3_color = combinePoints(source,target)
 
     # listOfRedPoints, listOfRedColors = sparse_subset3(p3_load, p3_color, 0.01)
-    listOfRedPoints, listOfRedColors = differencePoints(source, target, 0.07)
+    listOfRedPoints, listOfRedColors = differencePoints(source, target, 0)
 
     pcd = numpyToPC(listOfRedPoints,listOfRedColors)
+    pcd = ransacDB(pcd)
     o3d.visualization.draw_geometries([pcd])
     print("DONE")
